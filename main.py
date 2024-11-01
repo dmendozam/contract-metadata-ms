f29e0ecb125920ec46dd2483f3bdd54cedafe7ea
Cristian Triana <crisdevelop4@gmail.com>
2024-10-31 20:57:42 -0500

    Update python version

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv(override=True)
 
-if __name__ == "__main__":
-    from app.api.api import router
-    app = FastAPI(
-        title="metadata_ms",
-        description="Retrieves the metadata from the contract",
-        version="0.1.0"
-    )
-    app.include_router(router)
+from app.api.api import router
+app = FastAPI(
+    title="metadata_ms",
+    description="Retrieves the metadata from the contract",
+    version="0.1.0"
+)
+app.include_router(router)
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+uvicorn.run(app, host="0.0.0.0", port=8000)
     
